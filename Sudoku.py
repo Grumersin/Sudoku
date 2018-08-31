c3f30d564696d47bfa0572cced308461dee52db6
Grumersin <rbergofo10@alumnes.ub.edu>
2018-09-01 00:16:13 +0200

    Ja es poden posar els números inicials

--- Sudoku.py
+++ Sudoku.py
@@ -74,18 +74,53 @@ def graella(tor,mida):
     
 def posar_numero(x1,y1):
     global tor
+    global llista
     
     tor.penup()
-    tor.goto((x1+250)//50,(y1+250)//50)
+    tor.goto((x1+30)//60*60-10,(y1+30)//60*60-20)
     text=tl.textinput("Nombre","")
-    tor.write(text,False,"center",("Arial",30))
-    print(text)
+    if text=="exit": #Per quan vulgui parar de posar números i que ho resolgui l'ordi
+        return
+    else:
+        tor.write(text,False,"left",("Arial",30))
+        posx=int((x1+90)//180)
+        posy=int((y1+90)//180)
+        if posy==1:
+            posy=0
+        elif posy==0:
+            posy=3
+        elif posy==-1:
+            posy=6
+        posxx=int((x1+30)//60)
+        posyy=int((y1+30)//60)
+        if posxx<-1:
+            posxx=posxx+3
+        elif posxx>1:
+            posxx=posxx-3
+        if posyy<-1:
+            posyy=posyy+3
+        elif posyy>1:
+            posyy=posyy-3
+        if posyy==1:
+            posyy=0
+        elif posyy==0:
+            posyy=3
+        elif posyy==-1:
+            posyy=6
+        llista[1+posx+posy][1+posxx+posyy]=text
+        
+#def analitzar_columna(llista):
+    
+#def analitzar_fila(llista):
+    
+#def analitzar_quadrat(llista):
     
 
 fin=finestra()
 tor=punter()
 graella(tor,60)
-#posar_numero(xr,yr,punt)   
+
+llista=[[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0]]  
     
 fin.onscreenclick(posar_numero)
 #sudoku()
