aca0e488cdbaf5bd992e2ed06b5c461fcd56cf46
Grumersin <rbergofo10@alumnes.ub.edu>
2018-09-01 11:24:28 +0200

    Revisa files i quadrats i busca errors

--- Sudoku.py
+++ Sudoku.py
@@ -72,46 +72,77 @@ def graella(tor,mida):
     tor.forward(mida*9)
     tor.penup()
     
-def posar_numero(x1,y1):
-    global tor
-    global llista
-    
+def posar_numero(x1,y1,tor,llista):
     tor.penup()
     tor.goto((x1+30)//60*60-10,(y1+30)//60*60-20)
     text=tl.textinput("Nombre","")
-    if text=="exit": #Per quan vulgui parar de posar números i que ho resolgui l'ordi
-        return
+    tor.write(text,False,"left",("Arial",30))
+    posx=int((x1+90)//180)
+    posy=int((y1+90)//180)
+    if posy==1:
+        posy=0
+    elif posy==0:
+        posy=3
+    elif posy==-1:
+        posy=6
+    posxx=int((x1+30)//60)
+    posyy=int((y1+30)//60)
+    if posxx<-1:
+        posxx=posxx+3
+    elif posxx>1:
+        posxx=posxx-3
+    if posyy<-1:
+        posyy=posyy+3
+    elif posyy>1:
+        posyy=posyy-3
+    if posyy==1:
+        posyy=0
+    elif posyy==0:
+        posyy=3
+    elif posyy==-1:
+        posyy=6
+    llista[1+posx+posy][1+posxx+posyy]=text
+    
+def sudoku(xr,yr):
+    global tor
+    global llista
+    
+    if xr<60*4.5 and xr>-60*4.5 and yr<60*4.5 and yr>-60*4.5:
+        posar_numero(xr,yr,tor,llista)
     else:
-        tor.write(text,False,"left",("Arial",30))
-        posx=int((x1+90)//180)
-        posy=int((y1+90)//180)
-        if posy==1:
-            posy=0
-        elif posy==0:
-            posy=3
-        elif posy==-1:
-            posy=6
-        posxx=int((x1+30)//60)
-        posyy=int((y1+30)//60)
-        if posxx<-1:
-            posxx=posxx+3
-        elif posxx>1:
-            posxx=posxx-3
-        if posyy<-1:
-            posyy=posyy+3
-        elif posyy>1:
-            posyy=posyy-3
-        if posyy==1:
-            posyy=0
-        elif posyy==0:
-            posyy=3
-        elif posyy==-1:
-            posyy=6
-        llista[1+posx+posy][1+posxx+posyy]=text
+        for i in range(len(llista)): #per mirar tots els quadrats
+            analitzar_llista(llista[i]) #mira cada quadrat
+        for k in range(3): #mira 3 files de quadrats
+            for i in range(3): #mira 3 nombres d'un mateix quadrat
+                fila=[]
+                for j in range(3): #mira 3 quadrats consecutius
+                    fila[100:100]=llista[(j)+3*(k)][0+3*(i):3+3*(i)]
+#                print(fila)
+                analitzar_llista(fila)
         
 #def analitzar_columna(llista):
+#    for i in range(9):
     
-#def analitzar_fila(llista):
+'''def analitzar_fila(llista,n,m): #Revisar sencer, està a mig intentar
+    error=False
+    fila=[]
+    for n in range(9):
+        for i in range(3):
+            fila.append(llista[(i+n-2)][0*n:3*n])
+        for i in range(len(fila)):
+            for j in range(len(fila)):
+                if fila[i-1]==fila[j-1] and i!=j:
+                    error=True
+    print(error)'''
+    
+def analitzar_llista(llista):
+    for i in range(len(llista)):
+        for j in range(i):
+            if i!=j and llista[i-1]!=0  and llista[j-1]!=0:
+                if llista[i-1]==llista[j-1]:
+                    print(f"Error: el valor en la posició {i} ({llista[i-1]}) és igual al valor en la posició {j} ({llista[j-1]}).")
+                else:
+                    print("Tot correcte")
     
 #def analitzar_quadrat(llista):
     
@@ -122,7 +153,7 @@ graella(tor,60)
 
 llista=[[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0]]  
     
-fin.onscreenclick(posar_numero)
+fin.onscreenclick(sudoku)
 #sudoku()
  
 fin.listen()
