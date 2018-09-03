e4241791f68ae87fce1a3e19141d43b1710a0b43
Grumersin <rbergofo10@alumnes.ub.edu>
2018-09-03 13:44:40 +0200

    Resol per files, columnes i quadrats

--- Sudoku.py
+++ Sudoku.py
@@ -105,16 +105,79 @@ def posar_numero(x1,y1,tor,llista):
         tor.write(llista[1+posx+posy][1+posxx+posyy],False,"left",("Arial",30))
         tor.color("black")
     llista[1+posx+posy][1+posxx+posyy]=text
-    tor.write(int(text),False,"left",("Arial",30))
+    if text==0:
+        tor.color("white")
+        tor.write(int(text),False,"left",("Arial",30))
+        tor.color("black")
+    else:
+        tor.write(int(text),False,"left",("Arial",30))
+    
+def dibuixar(q,c,nom):    
+    tor.penup()
+    tor.home()
+    if q==0:
+        tor.setheading(135)
+        tor.forward(180*math.sqrt(2))
+    if q==1:
+        tor.setheading(90)
+        tor.forward(60*3)
+    if q==2:
+        tor.setheading(45)
+        tor.forward(180*math.sqrt(2))
+    if q==3:
+        tor.setheading(180)
+        tor.forward(60*3)
+    if q==5:
+        tor.forward(60*3)
+    if q==6:
+        tor.setheading(225)
+        tor.forward(180*math.sqrt(2))
+    if q==7:
+        tor.setheading(270)
+        tor.forward(60*3)
+    if q==8:
+        tor.setheading(315)
+        tor.forward(180*math.sqrt(2))
+    
+    if c==0:
+        tor.setheading(135)
+        tor.forward(60*math.sqrt(2))
+    if c==1:
+        tor.setheading(90)
+        tor.forward(60)
+    if c==2:
+        tor.setheading(45)
+        tor.forward(60*math.sqrt(2))
+    if c==3:
+        tor.setheading(180)
+        tor.forward(60)
+    if c==5:
+        tor.setheading(0)
+        tor.forward(60)
+    if c==6:
+        tor.setheading(225)
+        tor.forward(60*math.sqrt(2))
+    if c==7:
+        tor.setheading(270)
+        tor.forward(60)
+    if c==8:
+        tor.setheading(315)
+        tor.forward(60*math.sqrt(2))
+    tor.setheading(180+63.435)
+    tor.forward(10*math.sqrt(5))
+    tor.color("grey")
+    tor.write(nom,False,"left",("Arial",30))
+    tor.color("black")
+    llista[q][c]=nom
     
     
 def eliminar_possib(possib,i,j,n):
-    for m in range(9):
+    for m in range(9): #quadrat
         x=(n in possib[i][m])
         if x==True:
             possib[i][m].remove(n)        
     fila=[]
-    for m in range(3):
+    for m in range(3): #fila
         if i<3:
             k=0
         elif i>5:
@@ -136,7 +199,7 @@ def eliminar_possib(possib,i,j,n):
         if x==True:
             possib[fila[m][0]][fila[m][1]].remove(n)
     columna=[]
-    for m in range(3): #mira 3 quadrats diferents
+    for m in range(3): #columna
         if i==0 or i==3 or i==6:
             k=0
         if i==1 or i==4 or i==7:
@@ -175,7 +238,7 @@ def analitzar_llista(llista):
         for j in range(i):
             if i!=j and llista[i]!=0  and llista[j]!=0:
                 if llista[i]==llista[j]:
-                    print(f"Error: el valor en la posició {i} ({llista[i]}) és igual al valor en la posició {j} ({llista[j]}).")
+#                    print(f"Error: el valor en la posició {i} ({llista[i]}) és igual al valor en la posició {j} ({llista[j]}).")
                     suma+=1
     if suma!=0:
         error=True
@@ -236,137 +299,97 @@ def resoldre(llista,tor):
                     cont+=1
 #                print(f"possib:{possib[i][j]},{i},{j}")
                 if len(possib[i][j])==1 and cont==9:
-                    tor.penup()
-                    tor.home()
-                    if i==0:
-                        tor.setheading(135)
-                        tor.forward(180*math.sqrt(2))
-                    if i==1:
-                        tor.setheading(90)
-                        tor.forward(60*3)
-                    if i==2:
-                        tor.setheading(45)
-                        tor.forward(180*math.sqrt(2))
-                    if i==3:
-                        tor.setheading(180)
-                        tor.forward(60*3)
-                    if i==5:
-                        tor.forward(60*3)
-                    if i==6:
-                        tor.setheading(225)
-                        tor.forward(180*math.sqrt(2))
-                    if i==7:
-                        tor.setheading(270)
-                        tor.forward(60*3)
-                    if i==8:
-                        tor.setheading(315)
-                        tor.forward(180*math.sqrt(2))
-                    
-                    if j==0:
-                        tor.setheading(135)
-                        tor.forward(60*math.sqrt(2))
-                    if j==1:
-                        tor.setheading(90)
-                        tor.forward(60)
-                    if j==2:
-                        tor.setheading(45)
-                        tor.forward(60*math.sqrt(2))
-                    if j==3:
-                        tor.setheading(180)
-                        tor.forward(60)
-                    if j==5:
-                        tor.setheading(0)
-                        tor.forward(60)
-                    if j==6:
-                        tor.setheading(225)
-                        tor.forward(60*math.sqrt(2))
-                    if j==7:
-                        tor.setheading(270)
-                        tor.forward(60)
-                    if j==8:
-                        tor.setheading(315)
-                        tor.forward(60*math.sqrt(2))
-                    tor.setheading(180+63.435)
-                    tor.forward(10*math.sqrt(5))
-                    tor.color("grey")
-#                    tor.write(possib[i][j][0],False,"left",("Arial",30))
-                    tor.color("black")
-#                    llista[i][j]=possib[i][j][0]
-#                    eliminar_possib(possib,i,j,possib[i][j][0])
+                    dibuixar(i,j,possib[i][j][0])
+                    eliminar_possib(possib,i,j,possib[i][j][0])
                     
     for n in range(9): #quadrat
         for i in range(9): #els 9 nombres
-            sum=0
-            nom=0
+            sumx=0
+            sumy=0
+            sumz=0
+            nomx=0
+            nomy=0
+            nomz=0
             for j in range(9): #cel.la
-                x=(nombres[i] in possib[n][j])
+                x=(nombres[i] in possib[n][j]) #mira en el quadrat
                 if x==True:
-                    sum+=1
-                    q=n
-                    c=j
-                    nom=nombres[i]
-            print(f"possibilitats en el quadrat {n}: {possib[n]}")
-            print(nom,sum)
-            if sum==1:
-                print(nom,"únic", (q,c))
-                tor.penup()
-                tor.home()
-                if q==0:
-                    tor.setheading(135)
-                    tor.forward(180*math.sqrt(2))
-                if q==1:
-                    tor.setheading(90)
-                    tor.forward(60*3)
-                if q==2:
-                    tor.setheading(45)
-                    tor.forward(180*math.sqrt(2))
-                if q==3:
-                    tor.setheading(180)
-                    tor.forward(60*3)
-                if q==5:
-                    tor.forward(60*3)
-                if q==6:
-                    tor.setheading(225)
-                    tor.forward(180*math.sqrt(2))
-                if q==7:
-                    tor.setheading(270)
-                    tor.forward(60*3)
-                if q==8:
-                    tor.setheading(315)
-                    tor.forward(180*math.sqrt(2))
-                
-                if c==0:
-                    tor.setheading(135)
-                    tor.forward(60*math.sqrt(2))
-                if c==1:
-                    tor.setheading(90)
-                    tor.forward(60)
-                if c==2:
-                    tor.setheading(45)
-                    tor.forward(60*math.sqrt(2))
-                if c==3:
-                    tor.setheading(180)
-                    tor.forward(60)
-                if c==5:
-                    tor.setheading(0)
-                    tor.forward(60)
-                if c==6:
-                    tor.setheading(225)
-                    tor.forward(60*math.sqrt(2))
-                if c==7:
-                    tor.setheading(270)
-                    tor.forward(60)
-                if c==8:
-                    tor.setheading(315)
-                    tor.forward(60*math.sqrt(2))
-                tor.setheading(180+63.435)
-                tor.forward(10*math.sqrt(5))
-                tor.color("grey")
-                tor.write(nom,False,"left",("Arial",30))
-                tor.color("black")
-                llista[q][c]=nom
-                eliminar_possib(possib,q,c,nom)
-            print(f"possibilitats en el quadrat {n}: {possib[n]}")
+                    sumx+=1
+                    qx=n
+                    cx=j
+                    nomx=nombres[i]
+                fila=[]
+                for my1 in range(3): #fila
+                    if n<3:
+                        kx=0
+                    elif n>5:
+                        kx=6
+                    else:
+                        kx=3
+                    if j<3:
+                        l=0
+                    elif j>5:
+                        l=6
+                    else:
+                        l=3
+                    fila.append([kx+my1,0+l])
+                    fila.append([kx+my1,1+l])
+                    fila.append([kx+my1,2+l])
+            #    print(fila)
+                for my2 in range(len(fila)):
+                    y=(nombres[i] in possib[fila[my2][0]][fila[my2][1]])
+                    if y==True:
+                        sumy+=1
+                        qy=n
+                        cy=j
+                        nomy=nombres[i]
+                columna=[]
+                for mz1 in range(3): #columna
+                    if n==0 or n==3 or n==6:
+                        ky=0
+                    if n==1 or i==4 or n==7:
+                        ky=1
+                    if n==2 or n==5 or n==8:
+                        ky=2
+                    if j==0 or j==3 or j==6:
+                        l=0
+                    if j==1 or j==4 or j==7:
+                        l=1
+                    if j==2 or j==5 or j==8:
+                        l=2
+                    columna.append([mz1*3+ky,l])
+                    columna.append([mz1*3+ky,l+3])
+                    columna.append([mz1*3+ky,l+6])
+#                print(mz1,ky,mz1*3+k)
+            #    print(columna)
+                for mz2 in range(len(columna)):
+#                    print("mz2",mz2)
+#                    print("columna",columna)
+#                    print("columna[mz2]",columna[mz2])
+#                    print("possib[columna[mz2][0]]",possib[columna[mz2][0]])
+#                    print("len(possib[columna[mz2][0]])",len(possib[columna[mz2][0]]))
+                    z=(nombres[i] in possib[columna[mz2][0]][columna[mz2][1]])
+                    if z==True:
+                        sumz+=1
+                        qz=n
+                        cz=j
+                        nomz=nombres[i]               
+#            print(f"possibilitats en el quadrat {n}: {possib[n]}")
+#            print(nomx,sumx)
+            if sumx==1:
+#                print(nomx,"únic", (qx,cx))
+                dibuixar(qx,cx,nomx)
+                eliminar_possib(possib,qx,cx,nomx)
+#            print(f"possibilitats en el quadrat {n}: {possib[n]}")
+            if sumy==1:
+#                print(nomy,"únic", (qy,cy))
+                dibuixar(qy,cy,nomy)
+                eliminar_possib(possib,qy,cy,nomy)
+#            print(f"possibilitats en el quadrat {n}: {possib[n]}")
+            if sumz==1:
+#                print(nomz,"únic", (qz,cz))
+                dibuixar(qz,cz,nomz)
+                eliminar_possib(possib,qz,cz,nomz)
+#            print(f"possibilitats en el quadrat {n}: {possib[n]}")
 
 fin=finestra()
 tor=punter()
